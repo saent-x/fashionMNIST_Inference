a83c8670cbc5fc6fcc6a573f05f5dd2c10481de7
saent-x <torsxperience@outlook.com>
2024-10-08 13:44:58 +0100

    feat: added server setup code

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/saent-x/fashionMNIST_Inference/handlers"
+	"github.com/saent-x/fashionMNIST_Inference/pages"
+	"log"
+	"net/http"
+)
+
+func main() {
+	err := http.ListenAndServe(":3000", Setup())
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func Setup() http.Handler {
+	mux := http.NewServeMux()
+	//- Pages
+
+	handlers.Static(mux)
+	handlers.HandleUpload(mux)
+	pages.Home(mux)
+
+	fmt.Println("server started...")
+
+	return mux
+}
